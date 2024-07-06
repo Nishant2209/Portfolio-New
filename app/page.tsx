916817a94114c837b7883e034d6d616054ae2cb5
Nishant Kumar Singh <nishant220902@gmail.com>
2024-07-07 01:26:01 +0530

    create HOME page

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Clients />
+        <Experience />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
