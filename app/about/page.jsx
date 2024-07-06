fd6064ebde91f447720e8b4d5bff4463734681be
Nishant Kumar Singh <nishant220902@gmail.com>
2024-07-07 01:26:22 +0530

    create about page

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { navItems } from "@/data";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
+
+const About = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Grid />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default About;
