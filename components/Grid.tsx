722dec1cf70641e98f740897c4b3eead7fe7544c
Nishant Kumar Singh <nishant220902@gmail.com>
2024-07-07 02:08:27 +0530

    used aceternity grid for the hero section

---
+++ components/Grid.tsx
@@ -0,0 +1,31 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about" className="pb-10 py-32">
+      <h1 className="heading">
+        A glimpse <span className="text-purple">about me</span>
+      </h1>
+      <BentoGrid className="w-full mt-10">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            // remove icon prop
+            // remove original classname condition
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
