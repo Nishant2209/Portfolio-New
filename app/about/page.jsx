c35a903a53be03c1d4fad66e0f846d0e5d720c69
Nishant Kumar Singh <nishant220902@gmail.com>
2024-07-08 02:18:06 +0530

    remove Transition Component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,22 +6,18 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
 // import PersonalInfo from "@/components/PersonalInfo";
-import TransitionComponent from "@/components/ui/TransitionEffect";
 
 const About = () => {
   return (
-    <>
-      <TransitionComponent />
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-        <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems} />
-          <Grid />
-          <Approach />
-          {/* <PersonalInfo /> */}
-          <Footer />
-        </div>
-      </main>
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Grid />
+        <Approach />
+        {/* <PersonalInfo /> */}
+        <Footer />
+      </div>
+    </main>
   );
 };
 
--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -3,19 +3,15 @@
 import { navItems } from "@/data";
 import ContactUsPage from "@/components/ContactUsPage";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import TransitionComponent from "@/components/ui/TransitionEffect";
 
 const ContactUs = () => {
   return (
-    <>
-      <TransitionComponent />
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto">
-        <div className="w-full">
-          <FloatingNav navItems={navItems} />
-          <ContactUsPage />
-        </div>
-      </main>
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <div className="w-full">
+        <FloatingNav navItems={navItems} />
+        <ContactUsPage />
+      </div>
+    </main>
   );
 };
 
